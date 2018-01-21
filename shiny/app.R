5dcf715f02005fb0a1062fa806d57f0b31295215
eswarkrishna <eswarkrishna32@gmail.com>
2018-01-21 12:27:56 +0530

    First Shiny app

---
+++ shiny/app.R
@@ -0,0 +1,6 @@
+library(shiny)
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+print(str(bcl))
+ui <- fluidPage("BC Liquor Store", "prices")
+server <- function(input, output) {}
+shinyApp(ui = ui, server = server)
